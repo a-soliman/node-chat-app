e90f86118468fd97fb72fd423747318b190f4d1c
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-12 15:59:59 -0800

    configured the package

--- server/server.js
+++ server/server.js
@@ -2,17 +2,20 @@ const path		= require('path');
 const express	= require('express');
 
 
-const publicPath = path.join(__dirname,'../public');
+const publicPath 	= path.join(__dirname,'../public');
+const port			= process.env.PORT || 3000;
+
 const app = express();
 app.use(express.static(publicPath))
 
 app.get('/', ( req, res ) => {
 	res.send('working');
+
 });
 
 
-app.listen(3000, () => {
-	console.log('Server is working on port 3000')
+app.listen(port, () => {
+	console.log(`Server is working on port ${port}`)
 })
 
 
