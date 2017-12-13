971bff7dc9a97a94063679673e07b47c6e8c0aef
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-12 21:27:04 -0800

    coded generateMeassge method

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const http		= require('http');
 const express	= require('express');
 const socketIO	= require('socket.io');
 
+const { generateMessage } = require('./utils/messages');
+
+
 const publicPath 	= path.join(__dirname,'../public');
 const port			= process.env.PORT || 3000;
 
@@ -14,25 +17,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected!');
 
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'welcome newUser'
-	});
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'newUser joined!'
-	})
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'a new user Joined'))
 
 
 	socket.on('createMessage', ( message ) => {
 		console.log('sendMessage', message);
 
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		})
+		io.emit('newMessage', generateMessage( message.from, message.text ))
 		// socket.broadcast.emit('newMessage', {
 		// 	from: message.from,
 		// 	text: message.text,
