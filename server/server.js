cc21c16e4f0077e71390e906386b000ad82d2972
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-12 20:49:32 -0800

    greet new user and notify others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected!');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'welcome newUser'
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'newUser joined!'
+	})
+
+
 	socket.on('createMessage', ( message ) => {
 		console.log('sendMessage', message);
 
@@ -22,6 +33,11 @@ io.on('connection', (socket) => {
 			text: message.text,
 			createdAt: new Date().getTime()
 		})
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// })
 	})
 
 	socket.on('disconnect', () => {
