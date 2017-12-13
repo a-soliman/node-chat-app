440cfe81991eb66f1e2dfe297f9f09d698348970
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-12 19:44:19 -0800

    working with socket

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ const server = http.createServer(app);
 let io = socketIO(server)
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+	console.log('New user connected!');
 
+	socket.on('disconnect', () => {
+		console.log('Disconnected!')
+	})
+})
 
 
 server.listen(port, () => {
