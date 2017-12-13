02b38806de0a49e2781241b3696cad2d0a3e1cc9
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-12-13 09:32:01 -0800

    updated the UI, for better using exp

--- public/js/index.js
+++ public/js/index.js
@@ -13,17 +13,17 @@ socket.on('newMessage', function( message ) {
 	let list = $('#messages');
 	let li = $('<li></li>');
 	li.text(`${message.from}: ${message.text}`)
-	list.prepend(li)
+	list.append(li)
 });
 
 socket.on('newLocationMessage', ( message ) => {
 	let list = $('#messages');
 	let li = $('<li></li>');
 	let ankor = $('<a target="_blank">My current location</a>');
-	li.text(`${message.from}`);
+	li.text(`${message.from}: `);
 	ankor.attr('href', message.url);
 	li.append(ankor);
-	list.prepend(li);
+	list.append(li);
 })
 
 socket.on('disconnect', function() {
@@ -35,27 +35,39 @@ socket.on('disconnect', function() {
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
 
+	let textInput = $('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('[name=message]').val()
+		text: textInput.val()
 	}, function() {
-
+		clearInput(textInput);
 	})
+
 })
 
 let locationBtn = $('#send-location');
 locationBtn.on('click', () => {
 	if ( !navigator.geolocation ) {
 		return alert('Geolocation is not supported by your browser!')
-	}
+	};
+
+	locationBtn.attr('disabled', 'disabled').text('Sending Location...');
 
 	navigator.geolocation.getCurrentPosition(( position ) => {
+		locationBtn.removeAttr('disabled').text('Share Location');
+
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		})
 
 	}, () => {
-		alert('Unable to fetch location.')
+		alert('Unable to fetch location.');
+		locationBtn.removeAttr('disabled').text('Share Location');
 	})
 })
+
+function clearInput( input ) {
+	return input.val('');
+}
